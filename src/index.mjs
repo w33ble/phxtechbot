d3963da8b964e7ab44e6ffb5d38a874ab1d31772
joe fleming <joe.fleming@gmail.com>
2018-08-03 15:38:41 -0700

    feat: basic bot setup

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,27 @@
-export default function() {
-  // es6 module code goes here
-}
+import dotenv from 'dotenv';
+import Client from 'emiirc';
+import loginAndJoin from 'emiirc/src/plugins/login-join.mjs';
+import reconnect from 'emiirc/src/plugins/reconnect.mjs';
+import logger from './lib/logger.mjs';
+
+dotenv.config();
+
+const { NICK, USERNAME, PASSWORD, CHANNELS } = process.env;
+const client = new Client('irc.freenode.net', 6667, {
+  nick: NICK,
+  username: USERNAME,
+  pass: PASSWORD,
+});
+
+client.use(loginAndJoin(CHANNELS.split(',')));
+client.use(reconnect());
+
+// client.on('data', d => logger.log(`> ${d}`));
+
+client.on('join', ({ room }) => {
+  if (room === '#phxtech') logger.log('phxtech joined');
+});
+
+client.connect();
+
+export default null;
---
+++ src/lib/logger.mjs
@@ -0,0 +1,6 @@
+/* eslint no-console: 0 */
+export default {
+  log: (...args) => console.log(...args),
+  warn: (...args) => console.warn(...args),
+  error: (...args) => console.error(...args),
+};
